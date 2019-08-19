1f0ac0e4e046bd82e96eddf9f128b844c0aa8ae1
Erik <erik.zatloukal@proofreason.com>
2019-08-19 11:44:12 +0200

    Add prodution build

--- lib-esm/lib/Optimization.d.ts
+++ lib-esm/lib/Optimization.d.ts
@@ -3,5 +3,5 @@ import { MarkerProps } from "../components/Marker";
 import * as React from 'react';
 declare const useMarkerCache: (markerProps: MarkerProps[]) => any[];
 declare const updateMarkerCache: (markersCache: [React.ComponentElement<MarkerProps, null>[], React.Dispatch<React.ComponentElement<MarkerProps, null>[]>], id: number, props: MarkerProps, immutable?: boolean, forceUpdate?: boolean) => void;
-declare const useHideOutOfFovMarkers: (markers: google.maps.Marker[], map: google.maps.Map, callback?: () => void) => void;
+declare const useHideOutOfFovMarkers: (markers: google.maps.Marker[], map: google.maps.Map, callback?: () => void, active?: boolean) => void;
 export { useMarkerCache, updateMarkerCache, useHideOutOfFovMarkers };
